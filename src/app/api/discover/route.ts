784d797ab97a4d4b2fc8c4098ade3309a260f4eb
malmby <22050725+malmby@users.noreply.github.com>
2025-05-17 17:24:23 -0400

    Change: Discover sources

--- src/app/api/discover/route.ts
+++ src/app/api/discover/route.ts
@@ -1,10 +1,13 @@
 import { searchSearxng } from '@/lib/searxng';
 
 const articleWebsites = [
-  'yahoo.com',
+  'www.reuters.com',
   'www.exchangewire.com',
-  'businessinsider.com',
-  /* 'wired.com',
+  'www.propublica.org',
+  'www.politico.com',
+  /*'businessinsider.com',
+  'yahoo.com',
+  'wired.com',
   'mashable.com',
   'theverge.com',
   'gizmodo.com',
@@ -12,7 +15,7 @@ const articleWebsites = [
   'venturebeat.com', */
 ];
 
-const topics = ['AI', 'tech']; /* TODO: Add UI to customize this */
+const topics = ['Business', 'Markets', 'Tech', 'AI']; /* TODO: Add UI to customize this */
 
 export const GET = async (req: Request) => {
   try {
